173804ff5aa36f063dfa6a2ec1d6a66abcec1edf
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-01-09 19:53:35 +0800

    a test

--- test/groupy_test.rb
+++ test/groupy_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
+require 'groupy'
+
 class GroupyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  class Food
+
+    def initialize(dish)
+      @dish = dish
+    end
+    attr_reader :dish
+
+    include Groupy
+    groupy :dish do
+      group :healthy do
+        group :fruit do
+          value :apple
+          value :orange
+        end
+        value :rice
+      end
+
+      group :unhealthy do
+        value :fried_egg
+        value :bacon
+      end
+    end
   end
+  
+  test "groupies" do
+    groupy = Food.groupies[:dish]
+    assert_instance_of Groupy::OuterGroup, groupy
+  end
+
 end
